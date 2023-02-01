0aebf194cebe94486fd9846db93200b4dd4e309b
nastaran-shafie <nastaran.sh33@gmail.com>
2023-02-01 19:57:35 +0330

    feat:create Adjust function color

---
+++ Answer2/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import AdjustColor from './component/AdjustColor'
+
+function App() {
+
+  return (
+    <div className="App">
+   <AdjustColor/>
+    </div>
+  )
+}
+
+export default App
